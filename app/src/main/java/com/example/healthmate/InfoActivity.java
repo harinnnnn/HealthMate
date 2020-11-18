862a194d1388d84e3b3b1df64c64a52b1e705eb9
kanghanhee <kanghanhee59@gmail.com>
2020-11-18 18:42:56 +0900

    edit region

--- app/src/main/java/com/example/healthmate/InfoActivity.java
+++ app/src/main/java/com/example/healthmate/InfoActivity.java
@@ -1,9 +1,13 @@
 package com.example.healthmate;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +23,9 @@ import org.json.JSONObject;
 
 public class InfoActivity extends AppCompatActivity {
 
+    ArrayAdapter<CharSequence> adspin1, adspin2;
+    String choice_do="";
+    String choice_se="";
 
     private Button btn_info;
 
@@ -29,6 +36,7 @@ public class InfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) { // OnCreate는 액티비티 처음 실행될 때 실행되는 애들
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+
         userID = getIntent().getStringExtra("userID");
         userPass = getIntent().getStringExtra("userPass");
         userName = getIntent().getStringExtra("userName");
@@ -36,6 +44,152 @@ public class InfoActivity extends AppCompatActivity {
 //        userAge = Integer.parseInt(getIntent().getStringExtra("userAge"));
 
         btn_info = findViewById(R.id.btn_info);
+
+        final Spinner spin1 = (Spinner)findViewById(R.id.spinner);
+        final Spinner spin2 = (Spinner)findViewById(R.id.spinner2);
+//        Button btn_refresh = (Button)findViewById(R.id.btn_refresh);
+
+        adspin1 = ArrayAdapter.createFromResource(this, R.array.spinner_do, android.R.layout.simple_spinner_dropdown_item);
+        adspin1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spin1.setAdapter(adspin1);
+        spin1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if (adspin1.getItem(i).equals("서울특별시")) {
+                    choice_do = "서울특별시";//버튼 클릭시 출력을 위해 값을 넣었습니다.
+                    adspin2 = ArrayAdapter.createFromResource(InfoActivity.this, R.array.spinner_do_seoul, android.R.layout.simple_spinner_dropdown_item);
+                    adspin2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    spin2.setAdapter(adspin2);
+                    spin2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                        @Override
+                        public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                            choice_se = adspin2.getItem(i).toString();
+                        }
+
+                        @Override
+                        public void onNothingSelected(AdapterView<?> adapterView) {
+                        }
+                    });
+                } else if (adspin1.getItem(i).equals("인천광역시")) {
+                    choice_do = "인천광역시";
+                    adspin2 = ArrayAdapter.createFromResource(InfoActivity.this, R.array.spinner_do_incheon, android.R.layout.simple_spinner_dropdown_item);
+                    adspin2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    spin2.setAdapter(adspin2);
+                    spin2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                        @Override
+                        public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                            choice_se = adspin2.getItem(i).toString();
+                        }
+
+                        @Override
+                        public void onNothingSelected(AdapterView<?> adapterView) {
+                        }
+                    });
+                } else if (adspin1.getItem(i).equals("광주광역시")) {
+                    choice_do = "광주광역시";
+                    adspin2 = ArrayAdapter.createFromResource(InfoActivity.this, R.array.spinner_do_gwangju, android.R.layout.simple_spinner_dropdown_item);
+                    adspin2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    spin2.setAdapter(adspin2);
+                    spin2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                        @Override
+                        public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                            choice_se = adspin2.getItem(i).toString();
+                        }
+
+                        @Override
+                        public void onNothingSelected(AdapterView<?> adapterView) {
+                        }
+                    });
+                } else if (adspin1.getItem(i).equals("대구광역시")) {
+                    choice_do = "대구광역시";
+                    adspin2 = ArrayAdapter.createFromResource(InfoActivity.this, R.array.spinner_do_daegu, android.R.layout.simple_spinner_dropdown_item);
+                    adspin2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    spin2.setAdapter(adspin2);
+                    spin2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                        @Override
+                        public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                            choice_se = adspin2.getItem(i).toString();
+                        }
+
+                        @Override
+                        public void onNothingSelected(AdapterView<?> adapterView) {
+                        }
+                    });
+                } else if (adspin1.getItem(i).equals("울산광역시")) {
+                    choice_do = "울산광역시";
+                    adspin2 = ArrayAdapter.createFromResource(InfoActivity.this, R.array.spinner_do_ulsan, android.R.layout.simple_spinner_dropdown_item);
+                    adspin2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    spin2.setAdapter(adspin2);
+                    spin2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                        @Override
+                        public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                            choice_se = adspin2.getItem(i).toString();
+                        }
+
+                        @Override
+                        public void onNothingSelected(AdapterView<?> adapterView) {
+                        }
+                    });
+                } else if (adspin1.getItem(i).equals("대전광역시")) {
+                    choice_do = "대전광역시";
+                    adspin2 = ArrayAdapter.createFromResource(InfoActivity.this, R.array.spinner_do_daejeon, android.R.layout.simple_spinner_dropdown_item);
+                    adspin2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    spin2.setAdapter(adspin2);
+                    spin2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                        @Override
+                        public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                            choice_se = adspin2.getItem(i).toString();
+                        }
+
+                        @Override
+                        public void onNothingSelected(AdapterView<?> adapterView) {
+                        }
+                    });
+                } else if (adspin1.getItem(i).equals("부산광역시")) {
+                    choice_do = "부산광역시";
+                    adspin2 = ArrayAdapter.createFromResource(InfoActivity.this, R.array.spinner_do_busan, android.R.layout.simple_spinner_dropdown_item);
+                    adspin2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    spin2.setAdapter(adspin2);
+                    spin2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                        @Override
+                        public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                            choice_se = adspin2.getItem(i).toString();
+                        }
+
+                        @Override
+                        public void onNothingSelected(AdapterView<?> adapterView) {
+                        }
+                    });
+                } else if (adspin1.getItem(i).equals("대구광역시")) {
+                    choice_do = "대구광역시";
+                    adspin2 = ArrayAdapter.createFromResource(InfoActivity.this, R.array.spinner_do_daegu, android.R.layout.simple_spinner_dropdown_item);
+                    adspin2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    spin2.setAdapter(adspin2);
+                    spin2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                        @Override
+                        public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                            choice_se = adspin2.getItem(i).toString();
+                        }
+
+                        @Override
+                        public void onNothingSelected(AdapterView<?> adapterView) {
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+//        btn_refresh.setOnClickListener(new View.OnClickListener() {
+//            //버튼 클릭시 이벤트입니다.
+//            @Override
+//            public void onClick(View view) {
+//                Toast.makeText(InfoActivity.this, choice_do + "=" + choice_se, Toast.LENGTH_SHORT).show();
+//            }
+//        });
 
         btn_info.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,6 +199,9 @@ public class InfoActivity extends AppCompatActivity {
                 ChipGroup cg_gender = findViewById(R.id.cg_gender);
                 Chip chip_gender = (Chip) cg_gender.getChildAt(0);
                 user_sex = chip_gender.getText().toString(); // 이거 성별
+
+                //지역 받아오기
+
 
                 // 성격 담아오기
                 ChipGroup cg_personality = findViewById(R.id.cg_personality);
@@ -122,6 +279,6 @@ public class InfoActivity extends AppCompatActivity {
             }
         });
 
-
     }
+
 }
